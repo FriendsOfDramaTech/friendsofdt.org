25172481b7711af459f59457b73179bcc98da381
Michael Lee <mike.lee@c1tadel.com>
2020-11-19 20:39:41 -0800

    Tweaks
    * Add a join page to test the Mailchimp form
    * Fix e-mail adjuster script
    * Fix nav-menu color
    * Add tailing / to nav links

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('a[href^="#contact:"]').on('click', function (e) {
-    var self = $(this),
-        localPart = self.attr('href').replace(/^#contact:/, ''),
-        mailtoUrl = 'mailto:' + localPart + '@friendsofdt.org';
-    e.preventDefault();
-    window.open(mailtoUrl, '_self');
-  });
-});

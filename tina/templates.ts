6da90c85842dc35a98fc4734b61697565232e1d7
Michael Lee <mike.lee@c1tadel.com>
2024-03-10 20:16:27 -0700

    (fix) Adjust Tina Config

--- tina/templates.ts
+++ tina/templates.ts
@@ -5,16 +5,28 @@ export function news_postFields() {
       type: "string",
       name: "title",
       label: "title",
+      isTitle: true,
+      required: true,
+    },
+    {
+      type: "rich-text",
+      name: "body",
+      label: "Body of Document",
+      description: "This is the markdown body",
+      isBody: true,
     },
     {
       type: "datetime",
       name: "date",
       label: "date",
+      description: "When thu"
     },
     {
-      type: "datetime",
-      name: "publishdate",
-      label: "publishdate",
-    },
+      type: "boolean",
+      name: "draft",
+      label: "draft",
+      required: true,
+      description: "If this page should be published or not"
+    }
   ] as TinaField[];
 }
